26dffe5a30588f99a68a203b3c3d84c2efff373e
yujin <miru93198@gmail.com>
2023-03-06 20:44:40 +0900

    10430 18108

--- Algorithm_JS/Baekjoon/10430.js
+++ Algorithm_JS/Baekjoon/10430.js
@@ -1,4 +1,4 @@
-let input = require("fs").readFileSync("example.txt").toString().split(" ");
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 var A = parseInt(input[0]);
 var B = parseInt(input[1]);
 var C = parseInt(input[2]);
